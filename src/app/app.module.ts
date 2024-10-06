8959da68e4a6603f60dda2bef1100e4342f411d9
Lybnih <132515156+Lybnih@users.noreply.github.com>
2024-10-06 17:13:29 -0300

    Header

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FeedComponent } from './feed/feed.component';
+import { HeaderComponent } from './feed/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FeedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/feed/feed.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrl: './feed.component.css'
+})
+
+export class FeedComponent {
+
+}
